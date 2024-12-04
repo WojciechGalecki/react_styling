61b5da206c78c3012341a4328aa3e4014734dc8e
Wojciech Galecki <wojciech.galecki@wp.pl>
2024-12-04 13:59:24 +0100

    styled components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,4 +1,15 @@
 import { useState } from "react";
+import { styled } from "styled-components";
+import Button from "./Button";
+import CustomInput from "./Input";
+
+// .controls from 'index.css'
+const ControlContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -22,37 +33,29 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
-        <p className="paragraph">
-          <label className={`label ${emailNotValid ? "invalid" : ""}`}>
-            Email
-          </label>
-          <input
-            type="email"
-            className={emailNotValid ? "invalid" : undefined}
-            onChange={(event) => handleInputChange("email", event.target.value)}
-          />
-        </p>
-        <p>
-          <label className={`label ${passwordNotValid ? "invalid" : ""}`}>
-            Password
-          </label>
-          <input
-            type="password"
-            className={passwordNotValid ? "invalid" : undefined}
-            onChange={(event) =>
-              handleInputChange("password", event.target.value)
-            }
-          />
-        </p>
-      </div>
+      {/* <div className="controls"> */}
+      <ControlContainer>
+        <CustomInput
+          type="email"
+          label="Email"
+          invalid={emailNotValid}
+          onChange={(event) => handleInputChange("email", event.target.value)}
+        />
+        <CustomInput
+          type="password"
+          label="Password"
+          invalid={passwordNotValid}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+        />
+        {/* </div> */}
+      </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className="button" onClick={handleLogin}>
-          Sign In
-        </button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
